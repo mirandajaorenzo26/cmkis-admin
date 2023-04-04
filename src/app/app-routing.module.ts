b503635328b3fe0707e35b23aafaaf719b77e8a9
Jao Miranda <mirandajaorenzo@gmail.com>
2023-04-04 08:19:04 +0800

    Refractor Code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { KeyInventoryComponent } from './pages/key-inventory/key-inventory.component';
-import { FacultyComponent } from './pages/faculty/faculty.component';
-import { AttendanceCheckerComponent } from './pages/attendance-checker/attendance-checker.component';
-import { SemesterManagementComponent } from './pages/semester-management/semester-management.component';
-import { RoomKeyManagementComponent } from './pages/room-key-management/room-key-management.component';
-import { SettingsComponent } from './pages/settings/settings.component';
-import { AccountComponent } from './pages/account/account.component';
+import { DashboardComponent } from './feature/components/dashboard/dashboard.component';
+import { KeyInventoryComponent } from './feature/components/key-inventory/key-inventory.component';
+import { FacultyComponent } from './feature/components/faculty/faculty.component';
+import { AttendanceCheckerComponent } from './feature/components/attendance-checker/attendance-checker.component';
+import { SemesterManagementComponent } from './feature/components/semester-management/semester-management.component';
+import { RoomKeyManagementComponent } from './feature/components/room-key-management/room-key-management.component';
+import { SettingsComponent } from './feature/components/settings/settings.component';
+import { AccountComponent } from './feature/components/account/account.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
@@ -19,6 +19,7 @@ const routes: Routes = [
   {path: 'room-key-management', component: RoomKeyManagementComponent},
   {path: 'settings', component: SettingsComponent},
   {path: 'account', component: AccountComponent},
+  {path: '**', component: DashboardComponent } // Need new component for 404
 ];
 
 @NgModule({
